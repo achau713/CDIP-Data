6790ad1075c96ed275c6c9cd2e87e8bc38c552f9
Anthony Chau <achau713@gmail.com>
2020-03-08 14:17:24 -0700

    Try accessing data on CDIP THREDDS server

--- ImportData.py
+++ ImportData.py
@@ -1,3 +1,14 @@
-msg = "Hello World"
+import netCDF4
 
-print(msg)
+stn = '100'
+startdate = "04/01/2012" # MM/DD/YYYY
+enddate = "04/30/2012"
+
+
+# CDIP Archived Dataset URL
+data_url = 'http://thredds.cdip.ucsd.edu/thredds/dodsC/cdip/archive/' + stn + 'p1/' + stn + 'p1_historic.nc'
+
+test_archived_data = netCDF4.Dataset(data_url)
+
+print(test_archived_data)
+
