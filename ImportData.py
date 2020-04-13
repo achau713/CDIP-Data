05ff68c214a21e3a6acc71293d22b440c7f273f7
Anthony Chau <achau713@gmail.com>
2020-04-12 19:18:10 -0700

    Access CDIP Archive Data for 3 buoys closest to SoCal

--- ImportData.py
+++ ImportData.py
@@ -1,14 +1,31 @@
 import netCDF4
 
-stn = '100'
-startdate = "04/01/2012" # MM/DD/YYYY
-enddate = "04/30/2012"
-
+stn = '067'
+yeardate = '2011'
 
 # CDIP Archived Dataset URL
-data_url = 'http://thredds.cdip.ucsd.edu/thredds/dodsC/cdip/archive/' + stn + 'p1/' + stn + 'p1_historic.nc'
+# santaMonica028 = 'http://thredds.cdip.ucsd.edu/thredds/dodsC/cdip/archive/' + stn + 'p1/' + stn + 'p1_historic.nc'
 
-test_archived_data = netCDF4.Dataset(data_url)
+santaMonica028_archive = "https://thredds.cdip.ucsd.edu/thredds/dodsC/cdip/archive/028p1/028p1_historic.nc"
+sanPedroSouth213_archive = "https://thredds.cdip.ucsd.edu/thredds/dodsC/cdip/archive/213p1/213p1_historic.nc"
+longBeachChannel215_archive = "https://thredds.cdip.ucsd.edu/thredds/dodsC/cdip/archive/215p1/215p1_historic.nc"
 
-print(test_archived_data)
+
+santaMonica_archiveData = netCDF4.Dataset(santaMonica028_archive)
+sanPedroSouth_archiveData = netCDF4.Dataset(sanPedroSouth213_archive)
+longBeachChannel_archiveData = netCDF4.Dataset(longBeachChannel215_archive)
+
+
+print("List of variables")
+# print(nc.variables)
+
+
+# print metadata -> test data access
+print(santaMonica_archiveData.metadata_link)
+print(sanPedroSouth_archiveData.metadata_link)
+print(longBeachChannel_archiveData.metadata_link)
+
+
+
+
 
